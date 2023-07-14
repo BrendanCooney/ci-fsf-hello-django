ade5c5fb0c684a5f55a43c06622b090068ac4a62
BrendanCooney <advicast@gmail.com>
2023-07-14 06:59:43 +0000

    todo lis form created

--- todo/views.py
+++ todo/views.py
@@ -12,7 +12,7 @@ def get_todo_list(request):
     return render(request, 'todo/todo_list.html', context)
 
 def add_item(request):
-    if request.method = "POST" :
+    if request.method == 'POST' :
         name = request.POST.get('item_name')
         done = 'done' in request.POST
         Item.objects.create(name=name, done=done)
