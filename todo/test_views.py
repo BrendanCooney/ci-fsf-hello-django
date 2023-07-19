025783f87a158a9df122bcb9269bdc5052d4ef8b
BrendanCooney <advicast@gmail.com>
2023-07-19 08:44:26 +0000

    Adding test files for forms views and models

---
+++ todo/test_views.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from .models import Item
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
+
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test Todo Item')
+
+
+
+
+
+
+
+    
